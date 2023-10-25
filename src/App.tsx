bb7ce49fddae0164b72b2e8368f012ec8ba6f365
Thiago Kovnat <thiago.kovnat@creativecoefficient.net>
2023-10-25 15:48:56 -0300

    remove viteLogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import fiubaLogo from '/Logo-fiuba_big.png'
 import './App.css'
 import axios from "axios"
